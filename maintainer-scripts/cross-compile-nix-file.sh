2ce4ebe0875cb3675d3a01d998a37b2a60d12efe
Andrew Abbott <mirrexagon@mirrexagon.com>
2019-07-13 16:35:46 +1000

    Remove maintainer script

--- maintainer-scripts/cross-compile-nix-file.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Cross-compile a derivation from a Nix file.
-
-nix-build -E "with import <nixpkgs> { crossSystem = { config = \"aarch64-unknown-linux-gnu\"; }; }; callPackage $1 {}"
