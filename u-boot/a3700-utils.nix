3cec6e100652fb64d341f44ffb79a7f173e5c406
Andrew Abbott <mirrexagon@mirrexagon.com>
2019-07-13 19:03:19 +1000

    Do some ridiculous patching
    I don't even know what platforms are what anymore

--- u-boot/a3700-utils.nix
+++ u-boot/a3700-utils.nix
@@ -6,20 +6,20 @@ stdenv.mkDerivation {
   phases = [ "unpackPhase" "patchPhase" "installPhase" ];
 
   src = fetchFromGitHub {
-    owner = "MarvellEmbeddedProcessors";
+    owner = "mirrexagon";
     repo = "A3700-utils-marvell";
-    rev = "34ce2160a1521dda9c7c68e06fcde83242dee94a";
-    sha256 = "1x026rxknrnzlkaa79a13l26dl5rj1d6aa9dij13ir7pqpznfws6";
+    rev = "mrxgn-espressobin-v7";
+    sha256 = "0cjqf4p1gnm48la2rh09hdgmki8lf5x42dbd1jpb19334yc2m8x6";
   };
 
-  patches = [
-    ./a3700-utils-patches/0001-Added-tim-img-files-to-build-the-secondary-image.patch
-    ./a3700-utils-patches/0002-buildtim-add-argument-for-build-primary-or-secondary.patch
-    ./a3700-utils-patches/0003-git-add-clocks_ddr.txt-to-git-ignore-list.patch
-    ./a3700-utils-patches/0004-parser-add-preset_ddr_conf-field-for-using-preset-dd.patch
-    ./a3700-utils-patches/0005-add-.d-files-to-gitignore.patch
-    ./a3700-utils-patches/0006-ddr-update-ddr-topology-for-ddr3-ddr4.patch
-  ];
+  # patches = [
+  #   ./a3700-utils-patches/0001-Added-tim-img-files-to-build-the-secondary-image.patch
+  #   ./a3700-utils-patches/0002-buildtim-add-argument-for-build-primary-or-secondary.patch
+  #   ./a3700-utils-patches/0003-git-add-clocks_ddr.txt-to-git-ignore-list.patch
+  #   ./a3700-utils-patches/0004-parser-add-preset_ddr_conf-field-for-using-preset-dd.patch
+  #   ./a3700-utils-patches/0005-add-.d-files-to-gitignore.patch
+  #   ./a3700-utils-patches/0006-ddr-update-ddr-topology-for-ddr3-ddr4.patch
+  # ];
 
   # We just want the contents of the repo.
   installPhase = ''
--- u-boot/images.nix
+++ u-boot/images.nix
@@ -1,3 +1,5 @@
+# Needs to be built on x86-64 host, due to some build tools being checked-in x86-64 binaries...
+
 { stdenv, ubootEspressobin, armTrustedFirmwareEspressobin, a3700UtilsEspressobin, buildPackages }:
 
 stdenv.mkDerivation {
@@ -19,8 +21,20 @@ stdenv.mkDerivation {
 
     chmod -R 755 atf-marvell A3700-utils-marvell
 
-    substituteInPlace A3700-utils-marvell/script/getddrparams.sh \
-      --replace "/bin/bash" "${buildPackages.bash}/bin/bash"
+    # A3700-utils has some scripts it wants to run.
+    patchShebangs A3700-utils-marvell
+
+    # But a Perl script doesn't get patched.
+    substituteInPlace A3700-utils-marvell/ddr/tim_ddr/ddrparser.pl \
+      --replace "/usr/bin/perl" "${buildPackages.perl}/bin/perl"
+
+    # There are some binary tools in there that we have to patch.
+    # Since we're cross-compiling on x86-86 to aarch64, we need to use the host's
+    # CC to get the interpreter (can't use NIX_CC because its aarch64).
+    ${buildPackages.patchelf}/bin/patchelf --set-interpreter $(cat ${buildPackages.stdenv.cc}/nix-support/dynamic-linker) \
+      A3700-utils-marvell/ddr/tim_ddr/ddr3_tool
+    ${buildPackages.patchelf}/bin/patchelf --set-interpreter $(cat ${buildPackages.stdenv.cc}/nix-support/dynamic-linker) \
+      A3700-utils-marvell/ddr/tim_ddr/ddr4_tool
 
     cd atf-marvell
 
@@ -34,7 +48,7 @@ stdenv.mkDerivation {
     export CROSS_CM3=${buildPackages.gcc-arm-embedded}/bin/arm-none-eabi-
 
     # SPINOR ESPRESSObin DDR4 1CS 1GB
-    make DEBUG=1 USE_COHERENT_MEM=0 LOG_LEVEL=20 SECURE=0 CLOCKSPRESET=CPU_1000_DDR_800 DDR_TOPOLOGY=5 BOOTDEV=SPINOR PARTNUM=0 WTP=../A3700-utils-marvell/ PLAT=a3700 all fip
+    make DEBUG=1 USE_COHERENT_MEM=0 LOG_LEVEL=20 SECURE=0 CLOCKSPRESET=CPU_1000_DDR_800 DDR_TOPOLOGY=5 BOOTDEV=SPINOR PARTNUM=0 WTP=../A3700-utils-marvell PLAT=a3700 all fip
   '';
 
   installPhase = ''
